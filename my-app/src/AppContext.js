a6f7511d8e6076e6eef68f12a1dcc0f2555b8f1a
ozdemirilayda <ozdemirilayda95@gmail.com>
2023-11-27 23:13:20 +0300

    Initialize products

--- my-app/src/AppContext.js
+++ my-app/src/AppContext.js
@@ -1,10 +1,24 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import * as API from "./api";
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const initializeProducts = async () => {
+      try {
+        const productsData = await API.listProducts();
+        setProducts(productsData);
+      } catch (error) {
+        console.error(`Initializing products failed: ${error.message}`);
+      }
+    };
+
+    initializeProducts();
+  }, []);
 
   const addToCart = () => {
     console.log("add to cart");
@@ -14,7 +28,13 @@ const AppProvider = ({ children }) => {
     console.log("subtract from cart");
   };
 
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider
+      value={{ products, cartItems, addToCart, subtractFromCart }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export { AppProvider, AppContext };
