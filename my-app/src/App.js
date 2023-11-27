48d280a8804d741fcdf4bafbf22f4b17a7c7cfa3
ozdemirilayda <ozdemirilayda95@gmail.com>
2023-11-27 23:17:22 +0300

    Update app component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,22 +5,49 @@ import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
 
 import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import ProductCard from "./components/ProductCard";
 import SearchBar from "./components/SearchBar";
+import ProductList from "./components/ProductList";
 
-const cards = [1, 2, 3];
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative" sx={styles.AppBar}>
+        <Toolbar sx={styles.Toolbar}>
+          <SearchBar />
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div style={{ backgroundColor: "#f6f9fc" }}>
+          <ProductList />
+        </div>
+        <Box
+          sx={{
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Stack sx={{}} direction="row" spacing={2} justifyContent="center">
+              <Button variant="contained" sx={styles.Button}>
+                Load More...{" "}
+              </Button>
+            </Stack>
+          </Container>
+        </Box>
+      </main>
+    </>
+  );
+}
 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
-const useStyle = {
+export default App;
+
+const styles = {
   Button: {
     backgroundColor: "#C24B5A !important",
     "&:hover": {
@@ -35,45 +62,3 @@ const useStyle = {
     justifyContent: "center",
   },
 };
-function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-      <AppBar position="relative" sx={useStyle.AppBar}>
-        <Toolbar sx={useStyle.Toolbar}>
-          <SearchBar />
-        </Toolbar>
-      </AppBar>
-      <main>
-        <div style={{ backgroundColor: "#f6f9fc" }}>
-          <Container sx={{ py: 8 }} maxWidth="md">
-            {/* End hero unit */}
-            <Grid container spacing={4}>
-              {cards.map((card) => (
-                <Grid item key={card} xs={12} sm={6} md={4}>
-                  <ProductCard />
-                </Grid>
-              ))}
-            </Grid>
-          </Container>
-        </div>
-        <Box
-          sx={{
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Stack sx={{}} direction="row" spacing={2} justifyContent="center">
-              <Button variant="contained" sx={useStyle.Button}>
-                Load More...{" "}
-              </Button>
-            </Stack>
-          </Container>
-        </Box>
-      </main>
-    </ThemeProvider>
-  );
-}
-
-export default App;
