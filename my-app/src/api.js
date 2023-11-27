2b931a4f1c146939ef510a53c9a1ed79b5b1486d
ozdemirilayda <ozdemirilayda95@gmail.com>
2023-11-27 23:34:54 +0300

    Update api module
    createSession method is added

--- my-app/src/api.js
+++ my-app/src/api.js
@@ -1,13 +1,26 @@
 const BASE_URL =
   "https://linkedin-cv-crawler.beta-limited.workers.dev/interview";
 
+export const createSession = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/createsession`);
+    const data = await response.text();
+    return data;
+  } catch (error) {
+    const message = `Could not create session: ${error}`;
+    console.error(message);
+    throw new Error(message);
+  }
+};
+
 export const listProducts = async () => {
   try {
     const response = await fetch(`${BASE_URL}/products`);
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error(`Error fetching products: ${error}`);
-    throw error;
+    const message = `Could not fetch produts: ${error}`;
+    console.error(message);
+    throw new Error(message);
   }
 };
