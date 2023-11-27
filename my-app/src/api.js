408a82bd2f0793c4aa2996ee85da45f47f984b51
ozdemirilayda <ozdemirilayda95@gmail.com>
2023-11-28 00:42:47 +0300

    Update api functions

--- my-app/src/api.js
+++ my-app/src/api.js
@@ -24,3 +24,53 @@ export const listProducts = async () => {
     throw new Error(message);
   }
 };
+
+export const viewCart = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/view-cart`, {
+      method: "GET",
+      headers: {
+        "Session-ID": localStorage.getItem("sessionId"),
+      },
+    });
+
+    const contentType = response.headers.get("Content-Type");
+    if (contentType === "application/json") {
+      const data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    const message = `Could not fetch cart: ${error}`;
+    throw new Error(message);
+  }
+};
+
+export const addToCart = async (productId) => {
+  try {
+    await fetch(`${BASE_URL}/add-to-cart?id=${productId}`, {
+      method: "GET",
+      headers: {
+        "Session-ID": localStorage.getItem("sessionId"),
+      },
+    });
+    console.log(`product is added to cart`);
+  } catch (error) {
+    const message = `Could not add product to cart: ${error}`;
+    throw new Error(message);
+  }
+};
+
+export const subtractFromCart = async (productId) => {
+  try {
+    await fetch(`${BASE_URL}/subtract-from-cart?id=${productId}`, {
+      method: "GET",
+      headers: {
+        "Session-ID": localStorage.getItem("sessionId"),
+      },
+    });
+    console.log("product is subtracted from cart");
+  } catch (error) {
+    const message = `Could not subtract product from cart: ${error}`;
+    throw new Error(message);
+  }
+};
